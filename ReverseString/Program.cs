d69d0731c622c62acb00489a147f12aef371069a
PrathapKumar <PrathapKumar@DESKTOP-VM2DSUE>
2022-04-12 16:03:52 +0530

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿class ReverseString
+{
+    static void Main(string[] args)
+    {
+        string str, revString = "";
+        Console.WriteLine("Enter string : ");
+        str = Console.ReadLine();
+        str = str.ToLower();
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            revString = revString + str[i];
+        }
+        Console.WriteLine("Reverse String is : " + revString);
+        if (str == revString)
+        {
+            Console.WriteLine("\n String is palindrome");
+        }
+        else
+        {
+            Console.WriteLine("\n String is not palindrome.");
+        }
+        Console.ReadLine();
+    }
+}
